42c4f704c920b1c6d71cd60364113cb0c4c747ce
Ian Marshall <imarshall14@gmail.com>
2021-02-18 11:23:12 -0500

    commit tree f4608782267ac91a1e824c9caabf0ee636096b20

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,10 +1,7 @@
-class Student
+class Student < ActiveRecord::Base
 
     def to_s
         first_name+" "+last_name
     end
 
-    def self.create!(hash)
-        Student.create(:first_name, :last_name)
-    end
 end
